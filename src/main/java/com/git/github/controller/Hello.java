c91bd7e8ddeed200dd16ff67b388cf49f3d746ea
imad <imadajaj00@gmail.com>
2026-06-02 11:23:02 +0100

    Initial commit

---
+++ src/main/java/com/git/github/controller/Hello.java
@@ -0,0 +1,15 @@
+package com.git.github.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Hello {
+    
+    @GetMapping("/hello")
+    public String getMethodName() {
+        return "Hello World";
+    }
+    
+}
